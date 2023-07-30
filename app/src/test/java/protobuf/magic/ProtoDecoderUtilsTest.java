fc72aae5a415b4938af3252c40f4c9f115d453f8
michael2to3 <michael2ga3@gmail.com>
2023-07-30 14:33:24 +0300

    refactor: remove unused import in ProtoDecoderUtilsTest
    Remove the unused import statement in the ProtoDecoderUtilsTest class.

--- app/src/test/java/protobuf/magic/ProtoDecoderUtilsTest.java
+++ app/src/test/java/protobuf/magic/ProtoDecoderUtilsTest.java
@@ -1,7 +1,6 @@
 package protobuf.magic;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
 
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
